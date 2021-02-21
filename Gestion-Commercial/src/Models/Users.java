5997beddc71c08c3f9a21892160c8f47d9b9fcc4
unknown <moudjitaba69@gmail.com>
2021-02-21 17:12:47 +0100

    2eme livrable

---
+++ Gestion-Commercial/src/Models/Users.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Moudjitaba Barry et Kain Camara 
+ */
+public class Users extends Personne{
+   public String login;
+   public String password;
+
+    public Users() {
+        super();
+        type="Users";
+    }
+   
+}
+
